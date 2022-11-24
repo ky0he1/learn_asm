250917696f2207ea791c4c1b1ea5d8880a5f2d95
1rohas <kyouta1218@gmail.com>
2022-11-24 20:11:04 +0900

    dataセクションの利用

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,14 +1,19 @@
+; "hello, world"を標準出力
+
 section .data
 	msg	db 'hello, world', 0x0A
+	msg_len equ $-msg
+	sys_write equ 1
+	sys_exit equ 60
 
 section .text
 	global _start
 _start:
-	mov	rax, 1
+	mov	rax, sys_write
 	mov rdi, 1
 	mov rsi, msg
-	mov rdx, 13
+	mov rdx, msg_len
 	syscall
-	mov rax, 60
+	mov rax, sys_exit
 	mov rdi, 0
 	syscall
