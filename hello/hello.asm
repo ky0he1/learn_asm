545626992f7f8da53ee46878e58a49ebc568bad9
1rohas <kyouta1218@gmail.com>
2022-11-24 20:46:30 +0900

    コメント追記

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,29 +1,40 @@
 ; "hello, world"を標準出力
 
 section .data
-	; "hello, world"を定義
-	msg	db 'hello, world', 0x0A
-	; "hello, world"の文字数を定義
-	msg_len equ $-msg
+    ; "hello, world"を定義
+    msg	db 'hello, world', 0x0A
+    ; "hello, world"の文字数を定義
+    msg_len equ $-msg
 
-	; write システムコールを定義
-	sys_write equ 1
-	; write のファイルディスクリプタを定義(標準出力)
-	stdout_code equ 1
+    ; write システムコールを定義
+    sys_write equ 1
+    ; write のファイルディスクリプタを定義(標準出力)
+    stdout_code equ 1
 
-	; exit システムコールを定義
-	sys_exit equ 60
-	; exit のリターンコードを定義(正常終了)
-	return_code equ 0
+    ; exit システムコールを定義
+    sys_exit equ 60
+    ; exit のリターンコードを定義(正常終了)
+    return_code equ 0
 
 section .text
-	global _start
+    global _start
 _start:
-	mov	rax, sys_write
-	mov rdi, stdout_code
-	mov rsi, msg
-	mov rdx, msg_len
-	syscall
-	mov rax, sys_exit
-	mov rdi, return_code
-	syscall
+    ; write システムコールの処理
+    ; syscall番号の指定
+    mov	rax, sys_write
+    ; write の第1引数を定義
+    mov rdi, stdout_code
+    ; write の第2引数を定義
+    mov rsi, msg
+    ; write の第3引数を定義
+    mov rdx, msg_len
+    ; write システムコールを実行
+    syscall
+
+    ; exit システムコールの処理
+    ; syscall番号の指定
+    mov rax, sys_exit
+    ; exit の第1引数を定義
+    mov rdi, return_code
+    ; write システムコールを実行
+    syscall
