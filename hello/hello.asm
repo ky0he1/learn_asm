46748a8e55584ab0280fb28560a86d19989e06e8
1rohas <kyouta1218@gmail.com>
2022-11-24 20:38:05 +0900

    コメント追記

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,19 +1,29 @@
 ; "hello, world"を標準出力
 
 section .data
+	; "hello, world"を定義
 	msg	db 'hello, world', 0x0A
+	; "hello, world"の文字数を定義
 	msg_len equ $-msg
+
+	; write システムコールを定義
 	sys_write equ 1
+	; write のファイルディスクリプタを定義(標準出力)
+	stdout_code equ 1
+
+	; exit システムコールを定義
 	sys_exit equ 60
+	; exit のリターンコードを定義(正常終了)
+	return_code equ 0
 
 section .text
 	global _start
 _start:
 	mov	rax, sys_write
-	mov rdi, 1
+	mov rdi, stdout_code
 	mov rsi, msg
 	mov rdx, msg_len
 	syscall
 	mov rax, sys_exit
-	mov rdi, 0
+	mov rdi, return_code
 	syscall
